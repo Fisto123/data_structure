f6de461ead323e3b1f5fec271b948d0058b16940
fisto123 <oluwafisayoiyiola@@gmail.com>
2023-11-17 14:26:56 +0100

    data structure

---
+++ data_structure/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(MissingNumber(new int[] { 0, 1})); // EXPECTED [5]
+    }
+
+    static string MissingNumber(int[] nums)
+    {
+        List<int> missingList = new List<int>();
+        int minNum = nums.Min(); // 1
+
+        for (int i = minNum; i <= nums.Length + minNum; i++)
+        {
+            if (!nums.Contains(i))
+            {
+                missingList.Add(i);
+            }
+        }
+
+        return  string.Join("", missingList) ;
+    }
+}
